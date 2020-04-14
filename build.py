2d2cdb25eaaa956e079bbc5dfe7e0c496fb174e2
Josue Padilla <josue.padilla@thredup.com>
2020-04-14 18:16:55 -0500

    Added build and tests

---
+++ build.py
@@ -0,0 +1,7 @@
+import time
+
+print("Building app")
+for i in range(0, 10):
+    time.sleep(0.3)
+    print("...")
+print("Build complete")
---
+++ test.py
@@ -0,0 +1,7 @@
+import time
+
+print("Doing tests")
+for i in range(0, 10):
+    time.sleep(0.3)
+    print("...")
+print("Tests complete")
