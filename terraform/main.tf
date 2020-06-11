5eeb5032dd2308778ea1eb9bc7b200b33dc43776
Jason Edwards <jtanium@gmail.com>
2020-06-11 15:09:33 -0600

    Corrected port number

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  backend "remote" {
-    hostname = "my.scalr.com"
-    organization = "env-stst3lhp1fhvffo"
-    workspaces {
-      name = var.instance_name
-    }
-  }
-}
 
 module "linux_tweet_app" {
   source  = "my.scalr.com/env-stst3lhp1fhvffo/scalr_example/aws"
@@ -15,5 +6,5 @@ module "linux_tweet_app" {
   app_image = "jasonpayroc/linux_tweet_app"
   dns_name = "${var.instance_name}.payroc.com"
   instance_name = var.instance_name
-  instance_port = 8080
+  instance_port = 80
 }
